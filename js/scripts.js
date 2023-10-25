873e25b7d9c98152d50b2e7fab3c179ceae132a1
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-10-25 15:34:08 -0700

    Add UI and implement current code into it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,63 @@
-function pigLatin(phrase) {
+window.addEventListener("load", () => {
+    const form = document.querySelector("#pigLatin");
+    form.addEventListener("submit", pigLatin);
+
+});
+
+// UI Logic
+function displayAnswer(answer, p) {
+    answer.innerText = p;
+}
+
+// Business Logic
+function pigLatin(e) {
+    e.preventDefault();
+    const phrase = document.getElementById("word").value;
     const vowels = ["a", "e", "i", "o", "u"];
     let vowelResults = [];
     let letterArray = [];
     let consonantArray = [];
     let finalArray = [];
     let counter = 0;
+    let answer = document.getElementById("results");
     const phraseArray = phrase.split(" ");
 
     phraseArray.forEach((word) => {
         if (vowels.includes(word.charAt(0))) {
             vowelResults.push(word + "way");
-            console.log(vowelResults)
+            p = vowelResults.toString()
+            displayAnswer(answer, p);
         } else if (word.substring(0, 2) === "qu") {
             let newWord = word.slice(2);
             vowelResults.push(newWord + "quay");
-            console.log(vowelResults);
+            p = vowelResults.toString();
+            displayAnswer(answer, p);
         } else if (word.substring(1, 3) === "qu") {
             let newWord = word.slice(1);
             vowelResults.push(newWord + word[0] + "ay");
-            console.log(vowelResults);
+            p = vowelResults.toString();
+            displayAnswer(answer, p);
         } else {
             letterArray = word.split('')
-            //maybe not a forEach loop?
             letterArray.forEach((letter) => {
                 if (vowels.includes(letter) && counter === 0) {
                     counter++;
                     finalArray.push(letter);
                     letterArray = letterArray.slice(1);
-                    console.log("finalArray: " + finalArray + " consonantArray: " + consonantArray)
                 } else if (!vowels.includes(letter) && counter === 1) {
                     finalArray.push(letter);
-                    console.log("finalArray: " + finalArray + " consonantArray: " + consonantArray)
                 } else if (vowels.includes(letter) && counter === 1){
                     finalArray.push(letter);
                     letterArray = letterArray.slice(1);
-                    console.log("finalArray: " + finalArray + " consonantArray: " + consonantArray)
                 } else if (!vowels.includes(letter) && counter === 0){
                     consonantArray.push(letter);
                     letterArray = letterArray.slice(1);
-                    console.log("finalArray: " + finalArray + " consonantArray: " + consonantArray)
                 }
             });
             const finalArray2 = finalArray.concat(consonantArray)
             let result = finalArray2.join('');
-            console.log(result + "ay")
+            p = (result + "ay")
+            displayAnswer(answer, p);
         }
     });
 }
